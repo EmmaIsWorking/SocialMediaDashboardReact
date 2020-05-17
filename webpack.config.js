c6fefb7d30da352ce07043d3051d059097e407e5
EmmaIsWorking <dimple.pleod@gmail.com>
2020-05-17 15:06:57 -0500

    gh-pages dependence

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+}
